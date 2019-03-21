dfd0a4747cfc5d906b5833d7be13caa8e8c67964
Charlie <charleschengcy94@gmail.com>
2019-03-21 20:39:18 +0000

    登录时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -21,7 +21,7 @@ class SessionsController extends Controller
             'password' => 'required'
         ]);
 
-        if(Auth::attempt($credentials))
+        if(Auth::attempt($credentials, $request->has('remember')))
         {
             //登录成功后
             session()->flash('success', 'Welcome back!');
