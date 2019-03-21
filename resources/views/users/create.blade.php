926fd4ca60d47f59656fa83e908ba3917fadbc12
Charlie <charleschengcy94@gmail.com>
2019-03-21 02:14:24 +0000

    用户注册表单

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,5 +2,36 @@
 @section('title', 'Signup')
 
 @section('content')
-<h1>Signup</h1>
+<div class="offset-md-2 col-md-8">
+  <div class="card">
+    <div class="card-header">
+      <h5>Sign up</h5>
+    </div>
+    <div class="card-body">
+      <form method="POST" action="{{ route('users.store')}}">
+        <div class="form-group">
+          <label for="name">Name: </label>
+          <input type="text" name="name" class="form-control" value="{{old('name')}}">
+        </div>
+
+        <div class="form-group">
+          <label for="email">Email: </label>
+          <input type="text" name="name" class="form-control" value="{{old('email')}}">
+        </div>
+
+        <div class="form-group">
+          <label for="password">Password: </label>
+          <input type="text" name="name" class="form-control" value="{{old('password')}}">
+        </div>
+
+        <div class="form-group">
+          <label for="password_confirmation">Password confirmation: </label>
+          <input type="text" name="name" class="form-control" value="{{old('password_confirmation')}}">
+        </div>
+
+        <button type=submit class="btn btn-primary">Sign up</button>
+      </form>
+    </div>
+  </div>
+</div>
 @stop
