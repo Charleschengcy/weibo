8023a89179b8a9f0a9ab12aa4f86d721806517c2
Charlie <charleschengcy94@gmail.com>
2019-03-21 21:34:13 +0000

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Auth;
 
 class UsersController extends Controller
 {
@@ -36,5 +37,31 @@ class UsersController extends Controller
         session()->flash('success', 'Success, welcome to weibo');
 
         return redirect()->route('users.show', [$user]);
+    }
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $data=[];
+        $data['name'] = $request->name;
+
+        if($request->password)
+        {
+            $data['password'] = bcrypt($request->password);
+        }
+
+        $user->update($data);
+        session()->flash('success', 'You\'ve updated account');
+
+        return redirect()->route('users.show', $user);
     }
 }
