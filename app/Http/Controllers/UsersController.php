d703ab75b3fd149a31ade7d815912edbe364b625
Charlie <charleschengcy94@gmail.com>
2019-03-22 03:09:48 +0000

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,7 +11,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth', [
-            'except' => ['show', 'create', 'store']
+            'except' => ['show', 'create', 'store','index']
         ]);
 
         $this->middleware('guest', [
@@ -76,4 +76,28 @@ class UsersController extends Controller
 
         return redirect()->route('users.show', $user);
     }
+
+    public function index()
+    {
+        // $users = User::all();
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
