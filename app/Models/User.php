1dfe6a481b1555a0d8dfa95e3f580683a43c4853
Charlie <charleschengcy94@gmail.com>
2019-03-23 23:32:45 +0000

    modify user.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,6 +65,7 @@ class User extends Authenticatable
     public function sendPasswordResetNotification($token)
     {
         $this->notify(new ResetPassword($token));
+    }
 
     public function feed()
     {
