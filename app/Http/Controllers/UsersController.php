2508f50663b35e5ac46fee5212802fa78d18a79b
Charlie <charleschengcy94@gmail.com>
2019-03-24 03:47:46 +0000

    关注和粉丝列表页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -130,6 +130,22 @@ class UsersController extends Controller
         return back();
     }
 
+    public function followings(User $user)
+    {
+        $users = $user->followings()->paginate(10);
+        $title = $user->name.' following';
+
+        return view('users.show_follow', compact('users', 'title'));
+    }
+
+    public function followers(User $user)
+    {
+        $users = $user->followers()->paginate(10);
+        $title = $user->name.' follower';
+
+        return view('users.show_follow', compact('users', 'title'));
+    }
+
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,5 @@
 
  Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
 
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
