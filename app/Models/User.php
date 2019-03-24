0a5deb6ec53d97523743364ef5805d65136c2e80
Charlie <charleschengcy94@gmail.com>
2019-03-24 05:35:02 +0000

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -69,7 +69,12 @@ class User extends Authenticatable
 
     public function feed()
     {
-        return $this->statuses()->orderBy('created_at', 'desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        // $this->followings == $user->followings()->paginate()；
+        // $this->followings == $user->followings()->get()；
+        array_push($user_ids, $this->id);
+        // return $this->statuses()->orderBy('created_at', 'desc');
+        return Status::whereIn('user_id', $user_ids)->with('user')->orderBy('created_at', 'desc');
 
     }
 
